b3998de66109e9943f44834c86fac92191f39dc9
mike <mikeymaxdb@users.noreply.github.com>
2021-08-19 22:15:51 -0700

    fixed audio device name

--- config/awesome/widgets.lua
+++ config/awesome/widgets.lua
@@ -30,7 +30,7 @@ widgets.clock = clock
 
 -------------------------
 
-local audioDeviceName = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink"
+local audioDeviceName = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi___ucm0001.hw_sofhdadsp__sink"
 local volumeCommand = "pacmd list-sinks | grep -A 6 " .. audioDeviceName .. " | grep volume | cut -d '/' -f 2 | cut -d % -f 1"
 local muteCommand = "amixer sget Master | grep Mono: | awk '{print $(NF)}'"
 local volumeIcon= wibox.widget {
