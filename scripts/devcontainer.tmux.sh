6863f97c6ac10d18836378a8c3e8380b3e39d263
mike <mikeymaxdb@users.noreply.github.com>
2022-02-10 18:24:41 -0800

    dev container tmux script

---
+++ scripts/devcontainer.tmux.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+changeD(){
+    tmux send-keys "cd $1" 'C-m' 'C-l'
+}
+newWorkspace(){
+    changeD $1
+    tmux split-window -h
+    changeD $1
+    tmux split-window -v
+    changeD $1
+}
+
+tmux new-session -d -s dev
+
+newWorkspace '~/project'
+tmux rename-window 'open'
+
+tmux new-window
+newWorkspace '~'
+tmux rename-window 'open'
+
+tmux new-window
+newWorkspace '~'
+tmux rename-window 'open'
+
+tmux new-window
+newWorkspace '~'
+tmux rename-window 'open'
+
+tmux new-window
+newWorkspace '~'
+tmux rename-window 'open'
+
+tmux select-window -t 1
+tmux select-pane -t 1
+tmux send-keys 'e' 'C-m'
+
+tmux -2 attach-session -t work
