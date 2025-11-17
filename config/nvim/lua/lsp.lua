eb4b7413c90baafddfd33f25a3e527750e95db80
mike <mikeymaxdb@users.noreply.github.com>
2025-11-16 18:05:58 -0800

    minor cleanup

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -33,7 +33,7 @@ vim.diagnostic.config({
     signs = {
         text = {
             [vim.diagnostic.severity.ERROR] = '■',
-            [vim.diagnostic.severity.WARN]  = '▲'
+            [vim.diagnostic.severity.WARN]  = '▲',
             [vim.diagnostic.severity.HINT]  = '',
             [vim.diagnostic.severity.INFO]  = '◉',
         }
